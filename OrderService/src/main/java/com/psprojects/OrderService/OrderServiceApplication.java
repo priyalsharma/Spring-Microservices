b17cadc21f3b482ff419fdff00ad49120c13f5cb
priyalsharma <priyalsharma1296@gmail.com>
2024-02-01 16:52:28 -0600

    Implement security in Order Service

--- OrderService/src/main/java/com/psprojects/OrderService/OrderServiceApplication.java
+++ OrderService/src/main/java/com/psprojects/OrderService/OrderServiceApplication.java
@@ -1,21 +1,77 @@
 package com.psprojects.OrderService;
 
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
+import com.psprojects.OrderService.external.intercept.RestTemplateInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableFeignClients
 public class OrderServiceApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
+	@Autowired
+	private ClientRegistrationRepository clientRegistrationRepository;
+	@Autowired
+	private OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository;
+
+	/**
+	 * Adds interceptors as part of rest template
+	 * @return
+	 */
 		@Bean
 		@LoadBalanced
 		public RestTemplate restTemplate() {
-			return new RestTemplate();
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setInterceptors(
+				Arrays.asList(
+						new RestTemplateInterceptor(
+								clientManager(clientRegistrationRepository,
+										oAuth2AuthorizedClientRepository)
+						)
+				)
+		);
+		return restTemplate;
+		}
+
+	/**
+	 *
+	 * Adds interceptors as a part of feign client
+	 */
+		@Bean
+		public OAuth2AuthorizedClientManager clientManager(
+			ClientRegistrationRepository clientRegistrationRepository,
+			OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository){
+			OAuth2AuthorizedClientProvider oAuth2AuthorizedClientProvider
+					= OAuth2AuthorizedClientProviderBuilder
+					.builder()
+					.clientCredentials()
+					.build();
+			DefaultOAuth2AuthorizedClientManager oAuth2AuthorizedClientManager
+					= new DefaultOAuth2AuthorizedClientManager(
+							clientRegistrationRepository,
+							oAuth2AuthorizedClientRepository
+			);
+			oAuth2AuthorizedClientManager.setAuthorizedClientProvider(
+					oAuth2AuthorizedClientProvider
+			);
+			return oAuth2AuthorizedClientManager;
 		}
 }
