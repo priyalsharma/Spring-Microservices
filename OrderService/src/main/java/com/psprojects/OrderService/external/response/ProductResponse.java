84c747510703e8bc4fc161bdb30038bc91658751
priyalsharma <priyalsharma1296@gmail.com>
2024-02-25 12:16:31 -0600

    Complete working project: all service implementations with dockerfiles

---
+++ OrderService/src/main/java/com/psprojects/OrderService/external/response/ProductResponse.java
@@ -0,0 +1,17 @@
+package com.psprojects.OrderService.external.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponse {
+    private long productId;
+    private String productName;
+    private long price;
+    private long quantity;
+}
